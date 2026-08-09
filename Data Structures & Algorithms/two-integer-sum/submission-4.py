77b2015d37aff6cd367a220529aca23d123613f6
derrickyu07 <107325644+derrickyu07@users.noreply.github.com>
2026-08-09 16:29:28 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numDict = {}
+        for index, value in enumerate(nums):
+            if target - value in numDict:
+                return [numDict[target-value], index]
+            numDict[value] = index
+            
