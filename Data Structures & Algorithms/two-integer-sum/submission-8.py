c089046a1daf3ccf4768d90f88b04e7c6884bbc2
derrickyu07 <107325644+derrickyu07@users.noreply.github.com>
2026-08-30 17:38:07 -0400

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numDict = {}
+        for i,v in enumerate(nums):
+            if target - v in numDict:
+                return [numDict[target-v],i]
+            numDict[v] = i
